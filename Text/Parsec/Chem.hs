66887c04f41c3a4e62cfccdbcc2a51c1232c1c65
AngelitoJ <angeljalvarezmiguel@gmail.com>
2014-07-30 09:25:32 +0200

    Initial

---
+++ Text/Parsec/Chem.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Text.Parsec.Chem
+-- Copyright   :  (c) Angel Alvarez 2013, Felipe Zapata 2013 
+-- License     :  BSD-style (see the LICENSE file)
+--
+-- Maintainer  :  angel@uah.es
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-----------------------------------------------------------------------------
+
+
+module Text.Parsec.Chem
+    (
+      module Text.Parsec.Chem.Molcas
+    , module Text.Parsec.chem.Gaussian
+    ) where
